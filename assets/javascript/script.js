de5fa848f5ecb60e934e2724e615a45d3cf30665
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2022-11-12 16:20:46 -0800

    reorganized files and added questions

---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+//WHEN I click the start button
+//THEN a timer starts and I am presented with a question
+
+//WHEN I answer a question
+//THEN I am presented with another question
+
+//WHEN I answer a question incorrectly
+//THEN time is subtracted from the clock
+
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+
+//WHEN the game is over
+//THEN I can save my initials and score
+
+
+/*There are several things we need to complete this.
+We need a function for the timer.
+We need a function for randomly selecting a question.
+We need a function for randomly organizing the answers.
+We need to store the high-scores.*/
+
